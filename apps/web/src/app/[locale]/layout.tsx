f62eea376c23eba13ac370c0598e386d915fdf5e
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2026-09-08 18:31:52 -0300

    fix(web): widen editorial detail layouts

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -21,6 +21,7 @@ import "../editorial-method-dossier.css";
 import "../editorial-packs.css";
 import "../editorial-pack-blueprint.css";
 import "../editorial-pack-distribution.css";
+import "../editorial-detail-layout.css";
 import "../editorial-evidence.css";
 import "../editorial-evidence-report.css";
 import "../editorial-relations.css";
